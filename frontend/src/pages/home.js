ee3a989709ac0165bc40d170954ca2c5cbbc2d2e
Devansh Alok <devanshalok@gmail.com>
2022-11-25 01:17:15 -0800

    added a static homepage

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,23 @@
-import { Typography } from "@material-ui/core";
-
+import { Button } from "@material-ui/core";
+import "./page.css"
 function Home() {
-  return <Typography>About us page</Typography>;
+  return (
+    <section id='homeContainer' style={{backgroundColor:'#fff3e0'}}>
+    <img
+      src={require('./dream.png')}
+      alt='unsplash-img'
+    />
+    <div >
+      <h1>If studying abroad is your dream, making it simple is ours!</h1>
+      <p style={{fontSize:"20px"}}>Connect with thousands of students who will answer your questions</p>
+      <Button style={{
+        borderRadius: 35,
+        backgroundColor: "#ffa726",
+        padding: "18px 36px",
+        fontSize: "18px"
+    }}
+ variant="contained" href="#contained-buttons"> Get Started</Button>
+    </div>
+  </section>);
 }
 export default Home;
