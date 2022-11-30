ac3f264d9773ac490c70c0ddd34ca7152ddc62bb
Abul Hasan <abulhasan.swe@gmail.com>
2022-11-29 21:59:50 -0800

    Added new data

--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,16 +1,56 @@
-import React, { Component, Profiler } from 'react'
+import { useState } from 'react'
 import "./Login.css";
 import { Link } from "react-router-dom";
 import { Modal } from '@material-ui/core';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
+import axios from 'axios';
+import config from '../config/index';
+import { addProfile } from "../redux/userSlice";
 
-export default class SignUp extends Component {
-    
-    
-  render() {
-    return (
-        <div className="Login">
-      <form>
+import React from 'react'
+
+export default function Signup() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  
+  const token = useSelector(state => {
+    if (state.userSlice.profile && state.userSlice.profile.token) {
+      console.log('state is ',state.userSlice)
+      return state.userSlice.profile.token
+    } return undefined
+  })
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+  function handleSignup() {
+
+    axios.post(config.BASE_URL + '/signup', {
+      email,
+      password,
+      firstName,
+      lastName
+    }).then(response => {
+      if (response.status == 200 && response.data.statusCode == 200) {
+        console.log(response.data);
+        dispatch(addProfile({ ...response.data.data.userDetails, token: response.data.data.userDetails.token }));
+        navigate('/home')
+      } else {
+        console.log('some exception occurred', response)
+      }
+    }).catch(error => console.log('some exception occurred', error));
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
         <h3>Sign Up</h3>
 
         <div className="mb-3">
@@ -19,12 +59,15 @@ export default class SignUp extends Component {
             type="text"
             className="form-control"
             placeholder="First name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
           />
         </div>
 
         <div className="mb-3">
           <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
+          <input type="text" className="form-control" placeholder="Last name" value={lastName} onChange={(e) => setLastName(e.target.value)}
+/>
         </div>
 
         <div className="mb-3">
@@ -33,6 +76,8 @@ export default class SignUp extends Component {
             type="email"
             className="form-control"
             placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -42,6 +87,8 @@ export default class SignUp extends Component {
             type="password"
             className="form-control"
             placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
@@ -51,26 +98,28 @@ export default class SignUp extends Component {
             type="password"
             className="form-control"
             placeholder="Re-enter password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </div>
 
         <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary" onClick={handleSignup}>
             Sign Up
           </button>
         </div>
         <p className="forgot-password text-right">
           Already registered <Link to="/login" >
             Sign in?
-            </Link>
+          </Link>
         </p>
       </form>
-      </div>
-    )
-  }
+    </div>
+  )
 }
 
-//  profile page 
+
+//  profile page
 //  question ka Modal
 //  answer ka page
 //  explore spaces ka page
