fa541223d64ccfc65c8f99361e866de28613c102
Abul Hasan <abulhasan.swe@gmail.com>
2022-12-01 15:07:17 -0800

    message

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -9,6 +9,7 @@ const config = {
       // "http://28e8-2600-1700-65aa-d910-8070-2137-b6b-332f.ngrok.io"
       // "http://28e8-2600-1700-65aa-d910-8070-2137-b6b-332f.ngrok.io"
     // 'http://localhost:3001';
+    
     "http://54.219.183.119:3001"
   }
   
